50a0cb4f20022000b7b3690a591e18d08c4af7ce
David Blass <david@arktype.io>
2023-02-20 15:16:13 -0500

    feat: change inlay hint color to dark green

--- src/getColors.js
+++ src/getColors.js
@@ -77,7 +77,7 @@ module.exports = (palette) => ({
     "editorGroupHeader.noTabsBackground": null,
     "editorGroupHeader.tabsBackground": background,
     "editorGroupHeader.tabsBorder": background,
-    "editorInlayHint.foreground": "#00ff6a",
+    "editorInlayHint.foreground": "#2faf6460",
     "tab.activeBackground": shadeColor(background, 5),
     "tab.activeForeground": foreground,
     "tab.border": background,
